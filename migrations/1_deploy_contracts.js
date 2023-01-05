d1503a76ede10d8d4bf2ab19c91300ced08dd0fe
Энхтөр <enxtur.mon@gmail.com>
2023-01-06 00:34:13 +0800

    compiler optimizer enabled

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,9 @@
 const ERC20Wallet = artifacts.require('ERC20Wallet');
 const WalletFactory = artifacts.require('WalletFactory');
 
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(ERC20Wallet);
-  deployer.link(ERC20Wallet, WalletFactory);
-  deployer.deploy(WalletFactory);
+module.exports = async function (deployer, network, accounts) {
+  // await deployer.deploy(ERC20Wallet);
+  // await deployer.link(ERC20Wallet, WalletFactory);
+  await deployer.deploy(WalletFactory);
+  console.log('WalletFactory.address', WalletFactory.address);
 }
